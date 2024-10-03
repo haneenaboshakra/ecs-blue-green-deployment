53cfc0cc3aa8f315d6c7cf39eb8bee7ff1ee3442
Haneen <haneenaboshakra79@gmail.com>
2024-10-03 12:32:47 +0300

    all code

--- iac/pipeline/client/src/Auth/Sanitizer.jsx
+++
@@ -1,9 +0,0 @@
-// utils/sanitizer.js
-import DOMPurify from 'dompurify';
-
-export const sanitizeInput = (input) => {
-  if (typeof input === 'string') {
-    return DOMPurify.sanitize(input);
-  }
-  return input;
-};
--- iac/pipeline/client/src/index.js
+++ iac/pipeline/client/src/index.js
@@ -1,28 +1,28 @@
 import React from "react";
-import App from "./pages/App";
-import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-import { logToServer } from "./Logger/LogService";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+// import { createRoot } from "react-dom/client";
+// import { logToServer } from "./Logger/LogService";
 
-window.addEventListener('unhandledrejection', (event) => {
-  logToServer('error', 'Unhandled promise rejection', {
-    reason: event.reason,
-    promise: event.promise,
-  });
+// window.addEventListener('unhandledrejection', (event) => {
+//   logToServer('error', 'Unhandled promise rejection', {
+//     reason: event.reason,
+//     promise: event.promise,
+//   });
   
-  console.error('Unhandled promise rejection:', event);
-});
+//   console.error('Unhandled promise rejection:', event);
+// });
 
-window.onerror = (message, source, lineno, colno, error) => {
-  logToServer('error', message, { source, lineno, colno, error });
-};
+// window.onerror = (message, source, lineno, colno, error) => {
+//   logToServer('error', message, { source, lineno, colno, error });
+// };
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
 
-
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+document.write("Hello world!")
+// root.render(
+//   <BrowserRouter>
+//     {/* <App /> */}
+//   </BrowserRouter>
+// );
